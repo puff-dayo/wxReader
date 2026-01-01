5842d9d50e9c2f6583e0a18871296b5ab41763e3
Setsuna <84665734+puff-dayo@users.noreply.github.com>
2026-01-02 02:18:03 +0800

    add 0.0.1 to version number

--- src/wxReader.py
+++ src/wxReader.py
@@ -17,7 +17,7 @@ from wxReaderProvider import ContentProvider, PdfContentProvider, ArchiveContent
 from wxReaderView import PDFView
 
 APP_NAME = "wxReader"
-APP_VERSION = "1.3"
+APP_VERSION = "1.3.1"
 SUPPORTED_EXTENSIONS = {".pdf", ".epub", ".mobi", ".fb2", ".txt", ".zip", ".cbz"}
 SUPPORTED_EXTENSIONS_STRING = ";".join("*" + ext for ext in SUPPORTED_EXTENSIONS)
 SUPPORTED_WILDCARDS = f"Supported files ({SUPPORTED_EXTENSIONS_STRING})|{SUPPORTED_EXTENSIONS_STRING}|All files (*.*)|*.*"
